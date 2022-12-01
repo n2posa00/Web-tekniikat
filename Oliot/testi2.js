356ad8cae7e544dc8681ec18f949f6a8f40e5eb6
n2posa00 <n2posa00@students.oamk.fi>
2022-12-01 11:07:28 +0200

    Oliot testi 2 tehty

--- Oliot/testi2.js
+++ Oliot/testi2.js
@@ -2,4 +2,16 @@ let car = {
     plateNumber: "OOP-001",
     currentSpeed: 120 + "km/h"
 }
-console.log(car);
+
+document.querySelector("#car").textContent = "Plate number is: " +car.plateNumber + " " + "Current speed is: " + car.currentSpeed;
+
+let book = {
+    bookName: "Testi123",
+    releaseDate: "20.12.2008",
+    price: 19.99 + "$"
+}
+
+book.price = 25.00 + "$";
+
+document.querySelector("#book").textContent = "Book name is: " + book.bookName + " " + "It was relesed on: " + book.releaseDate + " " + "It's price is " + book.price;
+
